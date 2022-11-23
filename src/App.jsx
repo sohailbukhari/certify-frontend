dec6becf424e6c9a39099fefb80c723bcd8df6da
Sohail Bukhari <45048691+sohailbukhari@users.noreply.github.com>
2022-11-23 13:54:18 +0500

    feat: basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
+
+function RequireAuth({ children }) {
+  const logged_in = false;
+
+  let location = useLocation();
+
+  if (!logged_in) {
+    return <Navigate to='/login' state={{ from: location }} replace />;
+  }
+
+  return children;
+}
 
 function App() {
   return (
     <>
-      {/* <Login /> */}
-      <Register />
+      <Routes>
+        <Route path='/' element={<PublicPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route
+          path='/dashboard'
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }
+        />
+      </Routes>
     </>
   );
+}
+
+function Dashboard() {
+  return <div>Dashboard Page</div>;
+}
+
+function PublicPage() {
+  return <div>Landing Home Page</div>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
@@ -9,6 +10,8 @@ import 'react-toastify/dist/ReactToastify.min.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ToastContainer hideProgressBar={true} autoClose={1000} />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
