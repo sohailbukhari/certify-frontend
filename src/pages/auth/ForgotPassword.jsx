c61a7373c905edb79a8de8770e528823ec8ecb08
Sohail Bukhari <45048691+sohailbukhari@users.noreply.github.com>
2022-11-23 15:35:17 +0500

    fix: autocomplete

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -157,6 +157,7 @@ export default function ForgotPassword() {
                   id='password'
                   name='password'
                   type='password'
+                  autocomplete='new-password'
                   minlength='8'
                   required
                   className='relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
